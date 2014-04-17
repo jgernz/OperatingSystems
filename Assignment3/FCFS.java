6540738015ca9fc415ecb4c6ab3897eddd63244f
unknown <lambb88@HALSEY26.UWOAD.it.uwosh.edu>
2014-04-17 18:27:35 -0500

    initial first-come-first-serve class

---
+++ Assignment3/FCFS.java
@@ -0,0 +1,22 @@
+import java.lang.*;
+import java.util.*;
+import java.io.*;
+
+public class FCFS
+{
+	private int[] points;
+	private int length;
+	public FCFS(Process[] p){
+		//
+		this.length = p.size();
+
+
+	}
+	public void createQueue(){
+		//
+	}
+	public int[] getPoints(){
+		//
+		return points;
+	}
+}
