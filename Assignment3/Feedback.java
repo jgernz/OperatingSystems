0113bd90694bd46a3e7001c5aac77892c87245d1
schmic67 <schmic67@uwosh.edu>
2014-04-21 12:59:58 -0500

    Added start for Feedback class

---
+++ Assignment3/Feedback.java
@@ -0,0 +1,21 @@
+import java.lang.*;
+import java.util.*;
+import java.io.*;
+
+
+public class FCFS{
+
+	//4 queue levels for feedback loop
+	Queue<Process> time1 = new LinkedList<Process>();
+	Queue<Process> time2 = new LinkedList<Process>();
+	Queue<Process> time3 = new LinkedList<Process>();
+	Queue<Process> preemptive = new LinkedList<Process>();
+	
+	
+	public void generateFeedback(){
+	//method for feedback loop
+	
+	}
+
+}
+
