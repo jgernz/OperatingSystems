b1ff6d2b9274c525c67fd8198dfc10da8d58fb98
harshb98 <harshb98@uwosh.edu>
2014-04-21 21:17:21 -0500

    added more stuff

--- Assignment3/SRT.java
+++ Assignment3/SRT.java
@@ -26,6 +26,44 @@ public class SRT
             process.add(p[i]);
         }
         finProcs = SortAT(process);
+        
+        
+        while(process.isEmpty() != true)
+        {
+            boolean executed=false;
+            for (i=0;i<finProcs.size()-1 ;i++ ) 
+            {
+                // find the next process to execute //
+                if(process.get(i).getArrivalTime() < totalProcessTime)
+                {
+                    process.get(i).setWaitingTime(totalProcessTime - process.get(i).getArrivalTime());
+                    totalProcessTime = totalProcessTime + process.get(i).getServiceTime();
+                    finProcs.add(process.get(i));
+                    process.remove(i);
+                    executed=true;
+                    break;
+                }
+                if(process.get(i).getArrivalTime() == totalProcessTime)
+                {
+                    process.get(i).setWaitingTime(0);
+                    totalProcessTime = totalProcessTime + process.get(i).getServiceTime();
+                    finProcs.add(process.get(i));
+                    process.remove(i);
+                    executed=true;
+                    break;
+                }
+            }
+            // If executed == true then totalProcessTime has incremented already
+            // so no need to increment it again
+            if(executed==false)
+            {
+                totalProcessTime++;
+            }
+        }
+        
+        
+        
+        
         Process[] arr = finProcs.toArray(new Process[finProcs.size()]);
        return arr;
     }
