300f853630534331b49452b9535c8e93edd43dd6
harshb98 <harshb98@uwosh.edu>
2014-04-24 22:18:43 -0500

    somewhat working

--- Assignment3/SRT.java
+++ Assignment3/SRT.java
@@ -1,13 +1,3 @@
-    /*try and do the shortest remaining time
-
-
-    arrivalTime
-    serviceTime
-    timeRemaining
-    waitingTime
-    turnaroundTime
-    */
-
     import java.lang.*;
     import java.io.*;
     import java.util.*;
@@ -17,27 +7,46 @@
         int j = 0;
         int totalProcessTime = 0;
         ArrayList<Process> finProcs = new ArrayList<Process>();
-        
+            
         public Process[] shortProcSRT(Process[] p)
         {
             ArrayList<Process> process = new ArrayList<Process>();
-            for (i = 0; i<p.length-1; i++ ) 
+            for (i = 0; i<p.length; i++ ) 
             {
                 process.add(p[i]);
             }
-            process = SortAT(process);
-            
-            ArrayList<Process> tempRemaining = new ArrayList<Process>();
+
+            //Sort by arrival time
+            process = SortST(process);
+            //process = SortAT(process);
+            // Set total processing time to 0, it just started
+            totalProcessTime = 0;
+            // No wait so waiting time is 0
+            //process.get(0).setWaitingTime(0);
+            // Set active times
+            //process.get(0).setActiveTimes(0, process.get(0).getServiceTime());
+            // Increase total process time
+            //totalProcessTime = totalProcessTime + process.get(0).getServiceTime();
+            // Set turn around time
+            //process.get(0).setTurnaroundTime(totalProcessTime);
+            // Set Time Remaining
+            //process.get(0).calculateTimeRemaining(process.get(0).getServiceTime());
+            // Add to finished processes and remove from Process list //
+            //finProcs.add(process.get(0));
+            //process.remove(0);
+            // Keep executing until all processes have executed
             while(process.isEmpty() != true)
             {
                 // Keeps track if a Process was executed
                 boolean executed=false;
 
-                for (i=1;i<process.size()-1 ;i++ ) 
+                for (i=0;i<process.size() ;i++ ) 
                 {
-                    // find the next process to execute //
-                    if(process.get(i).getServiceTime() < process.get(i-1).getTimeRemaining())
+                   
+                    if(process.get(i).getServiceTime() == 0)
                     {
+                        System.out.println("process burst time = 0");
+
                             // Set waiting time
                         process.get(i).setWaitingTime(totalProcessTime - process.get(i).getArrivalTime());
 
@@ -57,16 +66,20 @@
                         finProcs.add(process.get(i));
                         
                         // Remove Process from Processes to be finished list
-                        /////process.remove(i);
-                        process = SortRT(process);
+                        process.remove(i);
+                        
                         // Process was executed - set to true
                         executed=true;
                         break;
                     }
-                    if(process.get(i).getArrivalTime() == totalProcessTime)
+
+
+                    //if(process.get(i).getServiceTime() < process.get(i+1).getServiceTime())
+                    if( interupt(process.get(i), process) == false)
                     {
+                        System.out.println("interupt = false");
                             // Set waiting time
-                        process.get(i).setWaitingTime(0);
+                        process.get(i).setWaitingTime(totalProcessTime - process.get(i).getArrivalTime());
 
                         // Set Active Times
                         process.get(i).setActiveTimes(totalProcessTime, (totalProcessTime + process.get(i).getServiceTime()));
@@ -84,10 +97,38 @@
                         finProcs.add(process.get(i));
                         
                         // Remove Process from Processes to be finished list
-                        if (process.get(i).getTimeRemaining() == 0)
-                        {
-                            process.remove(i);
-                        }
+                        process.remove(i);
+                        
+                        // Process was executed - set to true
+                        executed=true;
+                        break;
+                    }
+                    else
+                    {
+                        System.out.println("interupt = true");
+
+                            // Set waiting time
+                        process.get(i).setWaitingTime(totalProcessTime - process.get(i).getArrivalTime());
+
+                        // Set Active Times
+                        process.get(i).setActiveTimes(totalProcessTime, (totalProcessTime + process.get(i).getServiceTime()));
+
+                        // Increase totalProcessTime
+                        totalProcessTime = totalProcessTime + process.get(i).getServiceTime();
+
+                        // Set turn around time
+                        process.get(i).setTurnaroundTime(totalProcessTime + process.get(i).getArrivalTime());
+                        
+                        
+
+                        // Add Process to finished processes
+                        finProcs.add(process.get(i));
+                        // Set Time Remaining
+                        process.get(i).calculateTimeRemaining( process.get(i).getServiceTime() ) ;
+                        process.add( process.get(i) );
+                        // Remove Process from Processes to be finished list
+                        process.remove(i);
+                        
                         // Process was executed - set to true
                         executed=true;
                         break;
@@ -99,27 +140,37 @@
                 {
                     totalProcessTime++;
                 }
-            }     
-            Process[] arr = finProcs.toArray(new Process[finProcs.size()]);
-           return (arr);
+            }
+            // Change arraylist back to an Array and return it
+            Process[] returnProcs = finProcs.toArray(new Process[finProcs.size()]);
+            return ( returnProcs );
         }
-        
-        
 
 
 
-
-
-
-
-
+        public ArrayList<Process> SortST(ArrayList<Process> procSTsort)
+        {
+            for (j = 0; j<procSTsort.size(); j++ ) 
+            {
+                for (i = j+1; i<procSTsort.size(); i++ ) 
+                {
+                    if (procSTsort.get(j).getServiceTime() > procSTsort.get(i).getServiceTime())
+                    {
+                        Process temp = procSTsort.get(i);
+                        procSTsort.set(i, procSTsort.get(j) );
+                        procSTsort.set(j, temp);
+                    }   
+                }
+            }
+            return procSTsort;
+        }
 
 
         public ArrayList<Process> SortAT(ArrayList<Process> procATsort)
         {
-            for (j = 0; j<procATsort.size()-2; j++ ) 
+            for (j = 0; j<procATsort.size(); j++ ) 
             {
-                for (i = j+1; i<procATsort.size()-1; i++ ) 
+                for (i = j+1; i<procATsort.size(); i++ ) 
                 {
                     if (procATsort.get(j).getArrivalTime() > (procATsort.get(i)).getArrivalTime())
                     {
@@ -132,24 +183,40 @@
             return procATsort;
         }
 
-        public ArrayList<Process> SortRT(ArrayList<Process> procRTsort)
+        /*public boolean checkArray(<Process> p1, <Process> p2)
         {
-            for (j = 0; j<procRTsort.size()-2; j++ ) 
+            if ( (p1.getarrivalTime()) < (p2.getArrivalTime()+p2.getServiceTime()) && p1.getArrivalTime() > p2.getArrivalTime() )
             {
-                for (i = j+1; i<procRTsort.size()-1; i++ ) 
-                {
-                    if (procRTsort.get(j).getTimeRemaining() > (procRTsort.get(i)).getServiceTime())
-                    {
-                        Process temp = procRTsort.get(i);
-                        procRTsort.set(i, procRTsort.get(j) );
-                        procRTsort.set(j, temp);
-                    }   
-                }
+                if ( p1.getTimeRemaining() < p2.getTimeRemaining() )
+                    {return true;}
+                else
+                    {return false;}
             }
-            return procRTsort;
-        }
-        
-        
+            else
+                {return false;}
+
+
+        }*/
+
+        public boolean interupt(Process p1, ArrayList<Process> proc)
+        {
+            boolean bool = false;
+            for(int i = 0; i<proc.size(); i++)
+            {
+                if ( (proc.get(i).getArrivalTime()) < (p1.getArrivalTime()+p1.getServiceTime()) && p1.getArrivalTime() < proc.get(i).getArrivalTime() )
+                {
+                    System.out.println("inside first if");
+                    if ( p1.getTimeRemaining() < proc.get(i).getTimeRemaining() )
+                        {bool = true;}
+                    else
+                        {bool = false;}
+                }
+                else
+                    {bool = false;}
+            }
+            return bool;
+
+        }                      
     }
 
 
